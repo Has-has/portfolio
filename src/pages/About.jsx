709169e961a8dd1112bdec348c01755d860ca960
Hasan Abulaban <hasanabulaban92@gmail.com>
2026-06-14 15:53:40 -0400

    added featured project section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,7 @@ function About() {
       {/* LEFT IMAGE */}
       <div style={styles.left}>
         <img
-          src="profile.jpg" // you can replace later
+          src="../assets/profile.jpg" // you can replace later
           alt="about"
           style={styles.image}
         />
