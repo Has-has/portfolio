ac807c5e497d3b4dc59be9d055f063a131c3c985
Hasan Abulaban <hasanabulaban92@gmail.com>
2026-06-14 14:32:43 -0400

    added featured project section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,35 +3,29 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav style={styles.nav}>
-      {/* Logo */}
-      <h2 style={styles.logo}>Hasan</h2>
+  <div style={styles.inner}>
+    <h2 style={styles.logo}>Hasan</h2>
 
-      {/* Links */}
-      <div style={styles.links}>
-        <Link to="/" style={styles.link}>Home</Link>
-        <Link to="/about" style={styles.link}>About</Link>
-        <Link to="/projects" style={styles.link}>Portfolio</Link>
-        <Link to="/contact" style={styles.link}>Contact</Link>
-      </div>
-    </nav>
+    <div style={styles.links}>
+      <Link to="/" style={styles.link}>Home</Link>
+      <Link to="/about" style={styles.link}>About</Link>
+      <Link to="/projects" style={styles.link}>Portfolio</Link>
+      <Link to="/contact" style={styles.link}>Contact</Link>
+    </div>
+  </div>
+</nav>
   );
 }
 
 const styles = {
   nav: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    width: "100%",
-
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-
-    padding: "25px 10%",
-    backgroundColor: "transparent",
-    zIndex: 1000,
-  },
+  width: "100%",
+  backgroundColor: "#ffffff",
+  borderBottom: "1px solid #e5e7eb",
+  position: "sticky",
+  top: 0,
+  zIndex: 1000,
+},
 
   logo: {
     fontWeight: "600",
@@ -49,6 +43,15 @@ const styles = {
     textDecoration: "none",
     fontSize: "15px",
   },
+
+  inner: {
+  maxWidth: "1200px",
+  margin: "0 auto",
+  padding: "20px 40px",
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+},
 };
 
 export default Navbar;
